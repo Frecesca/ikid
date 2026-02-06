6787703ba8efc1f7d55cdd0aaa525b5682f09159
Frecesca <meixuw2@uw.edu>
2026-02-05 23:19:05 -0800

    Initial iKid project (UIKit + Storyboard)

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by 8888 on 2/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
